d9b41f66b29ab1bb066067b28acc80cedf38c7eb
Tanguy <lebouffant.tanguy@gmail.com>
2024-12-15 12:35:16 +0100

    Modification du main, tentative de régler le problème du dossier source

--- Source/main.py
+++ Source/main.py
@@ -1,3 +1,17 @@
-from .traits import TraitsManager
-from .interface import Interface
+import tkinter as tk
+from Source.traits import TraitsManager
+from Source.interface import Interface
 
+def main():
+    # Création du TraitsManager
+    manager = TraitsManager()
+
+    # Création de l'Interface en utilisant TraitsManager
+    interface = Interface(manager)
+
+    # Appel de la méthode pour créer l'interface graphique
+    interface.create_interface()
+
+# Point d'entrée du script principal
+if __name__ == "__main__":
+    main()
